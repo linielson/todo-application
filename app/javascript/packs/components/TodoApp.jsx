f00cb7baee88be931f04cdb50430554ed3c695e8
Linielson Rosa <linielsonrosa@gmail.com>
2020-08-20 21:36:57 -0300

    Filtering TodoItems

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -10,11 +10,13 @@ class TodoApp extends React.Component {
     super(props)
 
     this.state = {
-      todoItems: []
+      todoItems: [],
+      hideCompletedTodoItems: false
     }
 
     this.getTodoItems = this.getTodoItems.bind(this)
     this.createTodoItem = this.createTodoItem.bind(this)
+    this.toggleCompletedTodoItems = this.toggleCompletedTodoItems.bind(this)
   }
 
   componentDidMount() {
@@ -38,16 +40,26 @@ class TodoApp extends React.Component {
     this.setState({ todoItems });
   }
 
+  toggleCompletedTodoItems() {
+    this.setState({
+      hideCompletedTodoItems: !this.state.hideCompletedTodoItems
+    })
+  }
+
   render() {
     return (
       <>
         <TodoForm createTodoItem={this.createTodoItem} />
-        <TodoItems>
+        <TodoItems
+          toggleCompletedTodoItems={this.toggleCompletedTodoItems}
+          hideCompletedTodoItems={this.state.hideCompletedTodoItems}
+        >
           {this.state.todoItems.map(todoItem => (
             <TodoItem
               key={todoItem.id}
               todoItem={todoItem}
               getTodoItems={this.getTodoItems}
+              hideCompletedTodoItems={this.state.hideCompletedTodoItems}
             />
           ))}
         </TodoItems>
