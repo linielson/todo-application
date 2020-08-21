829089af996b57100912872c0ebff0afddf79afe
Linielson Rosa <linielsonrosa@gmail.com>
2020-08-20 21:30:24 -0300

    Debounce Requests

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import axios from "axios";
 import setAxiosHeaders from "./AxiosHeaders";
+import _ from "lodash"
 
 class TodoItem extends React.Component {
   constructor(props) {
@@ -20,11 +21,14 @@ class TodoItem extends React.Component {
   }
 
   handleChange() {
-    this.updateTodoItem();
+    this.setState({
+      complete: this.completedRef.current.checked
+    })
+
+    this.updateTodoItem()
   }
 
-  updateTodoItem() {
-    this.setState({ complete: this.completedRef.current.checked });
+  updateTodoItem = _.debounce(() => {
     setAxiosHeaders();
     axios
       .put(this.path, {
@@ -37,7 +41,7 @@ class TodoItem extends React.Component {
       .catch(error => {
         console.log(error);
       });
-  }
+  }, 1000)
 
   handleDestroy() {
     setAxiosHeaders();
